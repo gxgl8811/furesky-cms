7528edfec720c2d24530b598f73106371db857b9
jianda-desktop <jianda-desktop@163.com>
2019-08-13 23:58:56 +0800

    更新：数据库备份脚本

--- doc/backup_data.bat
+++ doc/backup_data.bat
@@ -1,33 +1,31 @@
 @echo off
 ::当前时间
-set now_time=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
-::备份到文件
-set file_name=F:\eclipse-workspace\furesky-cms\doc\furesky-cms.sql
-::MySQL
-set mysqldump=D:\DevTools\mysql-5.7.26\bin
-set host=127.0.0.1
-set port=3306
-set username=root
-set password=root
+set nowTime=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
+::当前目录
+set baseDir=%~dp0
+::MySQL数据库
 set database=furesky-cms
+::备份文件到
+set fileName=%baseDir%furesky-cms.sql
+
 
 echo.    
 echo -----------------------------------------------------------------
 echo                   MYSQL备份批处理
 echo                      by jiandax
 echo -----------------------------------------------------------------
-echo                  %now_time%
+echo                  %nowTime%
 echo.
 echo.
 echo 将备份到：
-echo     %file_name%
+echo     %fileName%
 echo.
 
 ::备份
 echo 开始备份...
 echo.
-cd /d %mysqldump%
-mysqldump --defaults-extra-file=../my.ini %database% > %file_name%
+cd /d %MySQL_HOME%/bin
+mysqldump --defaults-extra-file=%baseDir%mysql.ini %database% > %fileName%
 echo 备份结束...
 echo.
 echo 即将退出...
